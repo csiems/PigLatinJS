ef284885fb878c5a7f702f9d26fcfd4c028b3cc4
Christopher Siems and Patrick Sullivan <csiems@gmail.com>
2016-02-08 17:15:04 -0800

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,45 @@
 String.prototype.pigLatin = function() {
-  var consonants = /[^aeiou\W]+/i
-  var vowels = /[aeiou]+/i
-  var specialChars = /[^\s\w\']+(?=\w)/g;
-  this.replace(specialChars, "");
-  var words = this.split(" ");
-  
+  var consonants = /[^aeiou\W]+/i;
+  var vowels = /[aeiou]+/i;
+  var string = this;
+  string = string.replace(/-/g,"-@");
+  var words = string.split(/[\s\@]/);
   words.forEach(function(word, index) {
-    if(word[0].match(vowels)) {
-      words[index] = word + "ay";
-    } else if (word[0].match(consonants)) {
+    if(word[0] === "y") {
       var match = consonants.exec(word);
       word = word.substring(match[0].length);
       words[index] = word + match[0] + "ay";
+    } else if(word[0].match(vowels) && word[word.length - 1].match(vowels)) {
+      word = word + "yay"
+      words[index] = word;
+    } else if (word[0].match(vowels)) {
+      word = word + "ay"
+      words[index] = word;
+    } else if (word[0].match(consonants)) {
+      var match = /[^aeiouy\W]+/i.exec(word);
+      word = word.substring(match[0].length) + match[0] + "ay";
+      words[index] = word;
+    }
+
+    var punctuation = [".",",","?","!","-"," "];
+    for(var i = 0; i < word.length - 1; i++) {
+      if(punctuation.includes(word[i])) {
+        var temp = word[i];
+        word = word.slice(0,i) + word.slice(i+1) + temp;
+        words[index] = word;
+      }
     }
   });
-  return words.join(" ");
+  return words.join(" ").replace(/-\s/,"-");
 }
+
+$(function() {
+  $("#pig-latinify").on("click", function(e) {
+    e.preventDefault();
+    var userInput = $("#user-input").val();
+    var result = userInput.pigLatin();
+    $("#show-result").empty();
+    $("#show-result").append('<p>' + result + '</p>');
+    $("#user-input").val('');
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -11,4 +11,25 @@ describe('String', function() {
     expect("this string".pigLatin()).to.equal("isthay ingstray");
     expect("I'll take therapists for $500, Alex".pigLatin()).to.equal("I'llay aketay erapiststhay orfay $500, Alexay");
   });
+
+  it("if string starts with y count it as a consonant", function() {
+    expect("ystari".pigLatin()).to.equal("ariystay");
+  });
+
+  it("it handles vowels at beginning and end", function() {
+    expect("area".pigLatin()).to.equal("areayay");
+  });
+
+  it("it handles ys later in the word", function() {
+    expect("myrrh".pigLatin()).to.equal("yrrhmay");
+  });
+
+  it("handles punctuation correctly", function() {
+    expect("this.".pigLatin()).to.equal("isthay.");
+  })
+
+  it("handles hyphenated words correctly", function() {
+    expect("bi-mart".pigLatin()).to.equal("ibay-artmay");
+  })
+
 });
